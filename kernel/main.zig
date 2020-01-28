cd9b349d770ff5997f754b1323b6b70b26b2d800
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-01-28 14:27:18 +0100

    bugfixing

--- kernel/main.zig
+++ kernel/main.zig
@@ -14,12 +14,9 @@ export fn _start(magic: u32, info: [*c]tboot.tboot_info) callconv(.Naked) noretu
 pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace) noreturn {
     @setCold(true);
     var pixels = @intToPtr([*]volatile Color, @ptrToInt(term.terminal.buffer));
-    pixels[200] = Color{ .B = 0, .G = 0, .R = 255, .A = 0 };
-    pixels[201] = Color{ .B = 0, .G = 0, .R = 255, .A = 255 };
-    pixels[202] = Color{ .B = 0, .G = 0, .R = 255, .A = 255 };
-    pixels[203] = Color{ .B = 0, .G = 0, .R = 255, .A = 0 };
-    term.terminal.write("KERNEL PANIC: ");
-    term.terminal.write(msg);
+    pixels[200 + 640 * 6] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
+    term.terminal.write("A000A000A0000000000000000000000");
+    //term.terminal.write(msg);
     while (true) {}
 }
 const Color = packed struct {
@@ -28,20 +25,29 @@ const Color = packed struct {
     R: u8,
     A: u8,
 };
+
+fn draw2p(pixels: [*]volatile Color, n: u32, col: Color) void {
+    pixels[2 * n + (n / 640) * 640] = col;
+    pixels[2 * n + 640 + (n / 640) * 640] = col;
+    pixels[2 * n + 1 + (n / 640) * 640] = col;
+    pixels[2 * n + 641 + (n / 640) * 640] = col;
+}
+
 fn kmain(info: [*c]tboot.tboot_info) void {
     var pixels = @intToPtr([*]volatile Color, info.*.framebuffer.address);
+    var pixelsraw = @intToPtr([*]volatile u32, info.*.framebuffer.address);
 
     pixels[200] = Color{ .B = 255, .G = 0, .R = 0, .A = 0 };
     pixels[201] = Color{ .B = 255, .G = 0, .R = 0, .A = 0 };
 
     if (info.*.framebuffer.width > 2147483648) {
-        pixels[0] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
+        pixels[0] = Color{ .B = 255, .G = 255, .R = 255, .A = 0 };
     } else {
         pixels[0] = Color{ .B = 0, .G = 0, .R = 255, .A = 0 };
     }
 
     if (info.*.framebuffer.width == info.*.framebuffer.address) {
-        pixels[1] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
+        pixels[1] = Color{ .B = 255, .G = 255, .R = 255, .A = 0 };
     } else {
         pixels[1] = Color{ .B = 0, .G = 0, .R = 255, .A = 0 };
     }
@@ -52,7 +58,69 @@ fn kmain(info: [*c]tboot.tboot_info) void {
         pixels[2] = Color{ .B = 0, .G = 0, .R = 255, .A = 0 };
     }
 
-    var pixnum: u64 = (info.*.framebuffer.width) * (info.*.framebuffer.height);
+    if (info.*.framebuffer.height == 480) {
+        draw2p(pixels, 6, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 7, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 6 + 320, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 7 + 320, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+    } else {
+        draw2p(pixels, 6, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 7, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 6 + 320, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 7 + 320, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+    }
+    if (info.*.framebuffer.width == 640) {
+        draw2p(pixels, 8, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 9, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 8 + 320, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 9 + 320, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+    } else {
+        draw2p(pixels, 8, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 9, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 8 + 320, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 9 + 320, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+    }
+    if (info.*.framebuffer.pitch == 640) {
+        draw2p(pixels, 10, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 11, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 10 + 320, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+        draw2p(pixels, 11 + 320, Color{ .B = 255, .G = 255, .R = 255, .A = 0 });
+    } else {
+        draw2p(pixels, 10, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 11, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 10 + 320, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+        draw2p(pixels, 11 + 320, Color{ .B = 0, .G = 0, .R = 255, .A = 0 });
+    }
+
+    var dmp = @intToPtr([*]volatile u8, @ptrToInt(info));
+    var k: u32 = 0;
+    while (k < @bitSizeOf(tboot.tboot_info)) : (k += 1) {
+        var bit: u32 = dmp[k >> 3] >> @intCast(u3, k % 8);
+        bit &= 1;
+        if (bit == 1) {
+            pixelsraw[k + 3 * 640 + (k / 64) * (640 - 64)] = 0xFFFFFFFF;
+        } else {
+            pixelsraw[k + 3 * 640 + (k / 64) * (640 - 64)] = 0xFFFF0000;
+        }
+    }
+
+    if (@ptrToInt(&(info.*.framebuffer.address)) == @ptrToInt(&(info.*.framebuffer.width))) {
+        pixelsraw[50 * 640 + 4] = 0xFFFFFF00;
+    }
+    var dmpframe = @intToPtr([*]volatile u8, @ptrToInt(&(info.*.framebuffer.address)));
+    var k2: u32 = 0;
+    while (k2 < 32) : (k2 += 1) {
+        var bit: u32 = dmpframe[k2 >> 3] >> @intCast(u3, k2 % 8);
+        bit &= 1;
+        if (bit == 1) {
+            pixelsraw[k2 + 60 * 640 + (k2 / 64) * (640 - 64)] = 0xFFFFFFFF;
+        } else {
+            pixelsraw[k2 + 60 * 640 + (k2 / 64) * (640 - 64)] = 0xFF0000FF;
+        }
+    }
+
+    while (true) {}
+    var pixnum: u64 = (info.*.framebuffer.height) *% (info.*.framebuffer.pitch);
 
     pixels[200] = Color{ .B = 0, .G = 255, .R = 0, .A = 0 };
     pixels[201] = Color{ .B = 0, .G = 255, .R = 0, .A = 0 };
@@ -65,7 +133,7 @@ fn kmain(info: [*c]tboot.tboot_info) void {
     pixels[201] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
     pixels[202] = Color{ .B = 255, .G = 255, .R = 0, .A = 0 };
     pixels[203] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
-    term.terminal.buffer = @intToPtr([*]volatile u16, @ptrToInt(pixels));
+    term.terminal.buffer = @intToPtr([*]volatile u8, @ptrToInt(pixels));
     term.terminal.initialize();
-    term.terminal.write("Hello, kernel World!");
+    term.terminal.write("ZZZZ0000    ZZZZ0000");
 }
--- kernel/term.zig
+++ kernel/term.zig
@@ -1,30 +1,4 @@
 const builtin = @import("builtin");
-// Hardware text mode color constants
-const VgaColor = u8;
-const VGA_COLOR_BLACK = 0;
-const VGA_COLOR_BLUE = 1;
-const VGA_COLOR_GREEN = 2;
-const VGA_COLOR_CYAN = 3;
-const VGA_COLOR_RED = 4;
-const VGA_COLOR_MAGENTA = 5;
-const VGA_COLOR_BROWN = 6;
-const VGA_COLOR_LIGHT_GREY = 7;
-const VGA_COLOR_DARK_GREY = 8;
-const VGA_COLOR_LIGHT_BLUE = 9;
-const VGA_COLOR_LIGHT_GREEN = 10;
-const VGA_COLOR_LIGHT_CYAN = 11;
-const VGA_COLOR_LIGHT_RED = 12;
-const VGA_COLOR_LIGHT_MAGENTA = 13;
-const VGA_COLOR_LIGHT_BROWN = 14;
-const VGA_COLOR_WHITE = 15;
-
-fn vga_entry_color(fg: VgaColor, bg: VgaColor) u8 {
-    return fg | (bg << 4);
-}
-
-fn vga_entry(uc: u8, color: u8) u16 {
-    return @intCast(u16, uc) | (@intCast(u16, color) << 8);
-}
 
 const VGA_WIDTH = 80;
 const VGA_HEIGHT = 25;
@@ -32,31 +6,26 @@ const VGA_HEIGHT = 25;
 pub const terminal = struct {
     var row = @intCast(usize, 0);
     var column = @intCast(usize, 0);
-    var color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 
-    pub var buffer = @intToPtr([*]volatile u16, 0xB8000);
+    pub var buffer = @intToPtr([*]volatile u8, 0xB8000);
 
     pub fn initialize() void {
         var y = @intCast(usize, 0);
         while (y < VGA_HEIGHT) : (y += 1) {
             var x = @intCast(usize, 0);
             while (x < VGA_WIDTH) : (x += 1) {
-                putCharAt(' ', color, x, y);
+                putCharAt(' ', x, y);
             }
         }
     }
 
-    pub fn setColor(new_color: u8) void {
-        color = new_color;
-    }
-
-    pub fn putCharAt(c: u8, new_color: u8, x: usize, y: usize) void {
+    pub fn putCharAt(c: u8, x: usize, y: usize) void {
         const index = y * VGA_WIDTH + x;
-        buffer[index] = vga_entry(c, new_color);
+        buffer[index] = c;
     }
 
     pub fn putChar(c: u8) void {
-        putCharAt(c, color, column, row);
+        putCharAt(c, column, row);
         column += 1;
         if (column == VGA_WIDTH) {
             column = 0;
