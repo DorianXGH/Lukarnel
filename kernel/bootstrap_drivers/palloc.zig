b3dfad58b3e6b9319255f06b913a25df4726ee11
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-03-17 14:25:49 +0100

    Fix le bug d'allocation : les 400 premières pages sont maintenant réservées.

--- kernel/bootstrap_drivers/palloc.zig
+++ kernel/bootstrap_drivers/palloc.zig
@@ -14,66 +14,28 @@ pub const pallocator = struct {
         var maxpage = ((memsegs[count - 1].address + memsegs[count - 1].length) >> 12) - 1;
         var obitmap = @intToPtr([*]page_metadata, 1);
         var j: u64 = 0;
-        var pix: [*]u32 = @intToPtr([*]u32, 0x80000000);
-        pix[300] = 0x00FF0000;
         while (j < count) : (j += 1) {
-            pix[400 + 2 * j] = 0x000000FF;
-            pix[375] = 0x00FFFFFF;
-            pix[376] = 0x00FFFFFF;
-            pix[377] = 0x00FFFFFF;
-            pix[378] = 0x00FFFFFF * @intCast(u32, @boolToInt(memsegs[j].length > maxpage + 1));
-            pix[379] = 0x00FFFFFF;
-            pix[380] = 0x00FFFFFF * @intCast(u32, @boolToInt(memsegs[j].mtype == tboot.MEMORY_TYPE.USABLE));
-            pix[381] = 0x00FFFFFF;
             if (memsegs[j].mtype == tboot.MEMORY_TYPE.USABLE and memsegs[j].length > build_param.KERNEL_RESERVED and memsegs[j].length - build_param.KERNEL_RESERVED > maxpage + 1) {
-                pix[302] = 0x00FF0000;
                 obitmap = @intToPtr([*]page_metadata, memsegs[j].address + build_param.KERNEL_RESERVED);
                 var k: u64 = memsegs[j].address >> 12;
 
                 while (k < ((memsegs[j].address + build_param.KERNEL_RESERVED + memsegs[j].length) >> 12 + 1) and k < maxpage) : (k += 1) {
-                    if ((k % 600) < 300) {
-                        pix[400 + 2 * j] = 0x00FFFFF8F;
-                        pix[400 + 800 + 2 * j] = 0x00FFFFF8F;
-                    } else {
-                        pix[400 + 2 * j] = 0x00FFFF00;
-                        pix[400 + 800 + 2 * j] = 0x00FFFF00;
-                    }
                     obitmap[k] = page_metadata{ .owned = 0, .allocatable = 0, .other = 1 };
-                    pix[401 + 2 * j] = @intCast(u32, k);
                 }
             }
-            pix[401 + 2 * j] = 0x000000FF;
         }
-        pix[301] = 0x0000FF00;
-        pix[201] = 0x0000FF00;
-        pix[202] = 0x0000FF00;
-        pix[203] = 0x0000FF00;
         if (@ptrToInt(obitmap) != 0) {
-            pix[701] = 0x0000FF00;
-            pix[702] = 0x0000FF00;
-            pix[703] = 0x0000FF00;
-            pix[704] = 0x0000FF00;
             var i: u64 = 0;
             while (i < count) : (i += 1) {
-                pix[710] = 0x00FFFFFFF;
                 if (memsegs[i].mtype == tboot.MEMORY_TYPE.USABLE) {
-                    pix[714 + 3 * i] = 0x00FFFFFFF;
                     var k: u64 = (memsegs[i].address >> 12) + 1;
-                    pix[715 + 3 * i] = 0x00FFFFFFF * @intCast(u32, @boolToInt(k < (((memsegs[i].address + memsegs[i].length) >> 12) - 1)));
                     while (k < (((memsegs[i].address + memsegs[i].length) >> 12) - 1) and k < maxpage) : (k += 1) {
-                        pix[716 + 3 * i] = 0x00FF00FFF;
                         if (obitmap[k].other == 0)
                             obitmap[k] = page_metadata{ .owned = 0, .allocatable = 1, .other = 0 };
                     }
                 }
             }
-            pix[1400] = 0x00FFFF00;
-            pix[1401] = 0x00FFFFFF;
         } else {
-            pix[701] = 0x00FF0000;
-            pix[702] = 0x00FF0000;
-            pix[703] = 0x00FF0000;
-            pix[704] = 0x00FF0000;
             unreachable;
         }
         return pallocator{ .bitmap = obitmap, .maxpage = maxpage };
@@ -99,5 +61,11 @@ pub const pallocator = struct {
             }
         }
         unreachable;
+    }
+    pub fn preserve(self: pallocator, page: u64) void {
+        var reqpage: page_metadata = self.bitmap[page];
+        if (reqpage.allocatable == 1 and reqpage.owned == 0 and reqpage.other == 0) {
+            self.bitmap[page] = page_metadata{ .allocatable = 1, .other = 0, .owned = 1 };
+        }
     }
 };
--- kernel/main.zig
+++ kernel/main.zig
@@ -28,53 +28,50 @@ const Color = packed struct {
     A: u8,
 };
 
-fn draw2p(pixels: [*]volatile Color, n: u32, col: Color) void {
-    pixels[2 * n + (n / 640) * 640] = col;
-    pixels[2 * n + 640 + (n / 640) * 640] = col;
-    pixels[2 * n + 1 + (n / 640) * 640] = col;
-    pixels[2 * n + 641 + (n / 640) * 640] = col;
-}
-
 fn kmain(info: [*c]tboot.tboot_info) void {
     var pixels = @intToPtr([*]volatile Color, info.*.frmb_address);
 
-    var pixelsraw = @intToPtr([*]volatile u32, info.*.frmb_address);
-
     var pixnum: u64 = (info.*.frmb_height) *% (info.*.frmb_pitch);
 
-    pixels[200 + 640 * 6] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
-
     var page_allocator: palloc.pallocator = palloc.pallocator.init(info.*.mmap_entries, info.*.mmap_count);
+
     var spd: u64 = 0;
 
-    while (true) {
-        var i: u64 = 0;
-        while (i < pixnum) : (i += 1) {
-            pixels[i] = Color{ .R = @intCast(u8, (i * i) % 256), .G = @intCast(u8, (i * i * i + 76) % 256), .B = @intCast(u8, (i + 164) % 256), .A = 0 };
-        }
+    var resp: u64 = 0;
+    while (resp < 0x400) : (resp += 1) {
+        page_allocator.preserve(resp);
+    }
 
-        var ro: u64 = 0;
-        while (ro < 400) : (ro += 1) {
-            var co: u64 = 0;
-            while (co < 100) : (co += 1) {
-                pixels[spd + (co % info.*.frmb_width) + ro * info.*.frmb_pitch] = Color{ .R = 0, .G = 0, .B = 0, .A = 0 };
+    var interpix = @ptrCast([*]volatile Color, page_allocator.palloc(((info.*.frmb_pitch * info.*.frmb_height) >> 12) + 1));
+
+    var delayer: u64 = 0;
+    while (true) {
+        if (true) {
+            var i: u64 = 0;
+            while (i < pixnum) : (i += 1) {
+                interpix[i] = Color{ .R = @intCast(u8, (i * i) % 256), .G = @intCast(u8, (i * i * i + 76) % 256), .B = @intCast(u8, (i + 164) % 256), .A = 0 };
+            }
+
+            var ro: u64 = 0;
+            while (ro < 400) : (ro += 1) {
+                var co: u64 = 0;
+                while (co < 100) : (co += 1) {
+                    interpix[((spd + co) % info.*.frmb_width) + ro * info.*.frmb_pitch] = Color{ .R = 0, .G = 0, .B = 0, .A = 0 };
+                }
             }
         }
-        ro = 0;
-        while (ro < 400) : (ro += 1) {
-            var co: u64 = 0;
-            while (co < 100) : (co += 1) {
-                pixels[spd + (co % info.*.frmb_width) + ro * info.*.frmb_pitch] = Color{ .R = 0, .G = 0, .B = 0, .A = 0 };
-            }
+        var cop: u64 = 0;
+        while (cop < pixnum) : (cop += 1) {
+            pixels[cop] = interpix[cop];
         }
-        spd += 1;
+        delayer += 1;
+        if (delayer > 32) {
+            spd += 1;
+            delayer = 0;
+        }
         spd %= info.*.frmb_width;
     }
-    pixels[200] = Color{ .B = 255, .G = 255, .R = 0, .A = 0 };
-    pixels[201] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
-    pixels[202] = Color{ .B = 255, .G = 255, .R = 0, .A = 0 };
-    pixels[203] = Color{ .B = 255, .G = 0, .R = 255, .A = 0 };
-    term.terminal.buffer = @intToPtr([*]volatile u8, @ptrToInt(pixels));
+    term.terminal.buffer = @ptrCast([*]volatile u8, pixels); // in case something goes wrong and the loop breaks, it tries to display something.
     term.terminal.initialize();
     term.terminal.write("ZZZZ0000    ZZZZ0000");
 }
