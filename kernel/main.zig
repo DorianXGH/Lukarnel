5f652ad0ee35a99e31a97106b62186c8822111eb
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-03-17 14:43:54 +0100

    Commented main code

--- kernel/main.zig
+++ kernel/main.zig
@@ -29,31 +29,30 @@ const Color = packed struct {
 };
 
 fn kmain(info: [*c]tboot.tboot_info) void {
-    var pixels = @intToPtr([*]volatile Color, info.*.frmb_address);
+    var pixels = @intToPtr([*]volatile Color, info.*.frmb_address); // get an array of the pixels from the address given by the EFI information structure
 
-    var pixnum: u64 = (info.*.frmb_height) *% (info.*.frmb_pitch);
+    var pixnum: u64 = (info.*.frmb_height) *% (info.*.frmb_pitch); // get the number of pixels (as the are all 32 bits encoded, we can easily get the bytesize of the framebuffer)
 
-    var page_allocator: palloc.pallocator = palloc.pallocator.init(info.*.mmap_entries, info.*.mmap_count);
-
-    var spd: u64 = 0;
+    var page_allocator: palloc.pallocator = palloc.pallocator.init(info.*.mmap_entries, info.*.mmap_count); // initialize a page allocator with the memory map given by the EFI information structure
 
     var resp: u64 = 0;
-    while (resp < 0x400) : (resp += 1) {
+    while (resp < 0x400) : (resp += 1) { // reserve pages for kernel code
         page_allocator.preserve(resp);
     }
 
-    var interpix = @ptrCast([*]volatile Color, page_allocator.palloc(((info.*.frmb_pitch * info.*.frmb_height) >> 12) + 1));
+    var interpix = @ptrCast([*]volatile Color, page_allocator.palloc((pixnum >> 10) + 1)); // allocate pages for the second buffer (double buffering) (10 = 12 - 2, 12 for page, 2 because pixnum is in pixels of 4 bytes)
 
+    var spd: u64 = 0;
     var delayer: u64 = 0;
-    while (true) {
+    while (true) { // main loop
         if (true) {
             var i: u64 = 0;
-            while (i < pixnum) : (i += 1) {
+            while (i < pixnum) : (i += 1) { // funky colors YAY
                 interpix[i] = Color{ .R = @intCast(u8, (i * i) % 256), .G = @intCast(u8, (i * i * i + 76) % 256), .B = @intCast(u8, (i + 164) % 256), .A = 0 };
             }
 
             var ro: u64 = 0;
-            while (ro < 400) : (ro += 1) {
+            while (ro < 400) : (ro += 1) { // a moving black band because we need to move it, move it
                 var co: u64 = 0;
                 while (co < 100) : (co += 1) {
                     interpix[((spd + co) % info.*.frmb_width) + ro * info.*.frmb_pitch] = Color{ .R = 0, .G = 0, .B = 0, .A = 0 };
@@ -61,11 +60,11 @@ fn kmain(info: [*c]tboot.tboot_info) void {
             }
         }
         var cop: u64 = 0;
-        while (cop < pixnum) : (cop += 1) {
+        while (cop < pixnum) : (cop += 1) { // copy the second buffer to the memory mapped buffer
             pixels[cop] = interpix[cop];
         }
         delayer += 1;
-        if (delayer > 32) {
+        if (delayer > 32) { // slow the band, it moves too fast, sometimes.
             spd += 1;
             delayer = 0;
         }
