ac115d5168dd8ca7f5200cda1016e54662e5dc47
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-04-25 21:48:48 +0200

    began adding multiboot 2 structures

---
+++ kernel/tboot/multiboot2.zig
@@ -0,0 +1,16 @@
+pub const MB2_MAGIC: u32 = 0xE85250D6;
+
+pub const mb2_header = packed struct {
+    magic: u32,
+    arch: u32,
+    header_len: u32,
+    checksum: u32,
+    pub fn mb2_header(arch: u32, header_len: u32) mb2_header {
+        return mb2_header{
+            .magic = MB2_MAGIC,
+            .arch = arch,
+            .header_len = header_len,
+            .checksum = 0xFFFFFFFF - (MB2_MAGIC +% arch +% header_len),
+        };
+    }
+};
