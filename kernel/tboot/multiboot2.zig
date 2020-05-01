f757f78dd039467e4350ab0722d18534af37cc82
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-05-01 18:48:10 +0200

    added ir tag + comptime checks

--- kernel/tboot/multiboot2.zig
+++ kernel/tboot/multiboot2.zig
@@ -6,11 +6,26 @@ pub const mb2_header = packed struct {
     header_len: u32,
     checksum: u32,
     pub fn mb2_header(arch: u32, header_len: u32) mb2_header {
-        return mb2_header{
-            .magic = MB2_MAGIC,
-            .arch = arch,
-            .header_len = header_len,
-            .checksum = (0xFFFFFFFF - (MB2_MAGIC +% arch +% header_len)) + 1,
-        };
+        comptime {
+            return mb2_header{
+                .magic = MB2_MAGIC,
+                .arch = arch,
+                .header_len = header_len,
+                .checksum = (0xFFFFFFFF - (MB2_MAGIC +% arch +% header_len)) + 1,
+            };
+        }
+    }
+};
+
+pub const mb2_ir_tag = struct {
+    pub fn make_ir_tag_type(tags: [:0]u32) type { // make information request tag type
+        comptime {
+            return packed struct {
+                tag_type: u16 = 1,
+                flags: u16,
+                size: u32 = 8 + 4 * len(tags),
+                requested_info_tags: [len(tags)]u32 = tags,
+            };
+        }
     }
 };
