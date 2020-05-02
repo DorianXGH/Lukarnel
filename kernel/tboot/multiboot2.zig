ffeeafd9372b610f69088936ff60856b75c9ab4d
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-05-02 21:56:40 +0200

    added more mb2 tags

--- kernel/tboot/multiboot2.zig
+++ kernel/tboot/multiboot2.zig
@@ -1,3 +1,5 @@
+const tboot = @import("./tboot.zig");
+
 pub const MB2_MAGIC: u32 = 0xE85250D6;
 
 pub const mb2_header = packed struct {
@@ -17,15 +19,60 @@ pub const mb2_header = packed struct {
     }
 };
 
-pub const mb2_ir_tag = struct {
-    pub fn make_ir_tag_type(tags: [:0]u32) type { // make information request tag type
-        comptime {
-            return packed struct {
-                tag_type: u16 = 1,
-                flags: u16,
-                size: u32 = 8 + 4 * len(tags),
-                requested_info_tags: [len(tags)]u32 = tags,
-            };
+pub fn mb2_ir_tag(tags: []u32) type { // make information request tag type
+    comptime {
+        return packed struct {
+            tag_type: u16 = 1,
+            flags: u16,
+            size: u32 = 8 + 4 * len(tags),
+            requested_info_tags: [len(tags)]u32 = tags,
+        };
+    }
+}
+
+pub const mb2_framebuffer_req_tag = packed struct {
+    tag_type: u16 = 5,
+    flags: u16,
+    size: u32 = 20,
+    width: u32 = 1080,
+    height: u32 = 720,
+    depth: u32 = 32,
+};
+
+pub const mb2_info_header = packed struct {
+    total_size: u32,
+    reserved: u32 = 0,
+};
+
+pub const mb2_info_tag_header = packed struct {
+    tag_type: u32,
+    size: u32,
+};
+
+pub const mb2_info_memmap_base = packed struct {
+    tag_type: u32 = 6,
+    size: u32,
+    entry_size: u32 = 24,
+    entry_version: u32 = 0,
+    pub const mb2_info_memmap_entry = packed struct {
+        base_addr: u64,
+        length: u64,
+        mtype: tboot.MEMORY_TYPE,
+        reserved: u32 = 0,
+    };
+    pub fn get_entries(self: mb2_info_memmap_base) [*]mb2_info_memmap_entry {
+        if (entry_version == 0) {
+            return @intToPtr([*]mb2_info_memmap_entry, @ptrToInt(u64, &self) + 16);
+        } else {
+            unreachable;
+        }
+    }
+    pub fn get_length(self: mb2_info_memmap_base) u64 {
+        if (entry_version == 0) {
+            var n: u64 = (self.size - 16) / entry_size;
+            return n;
+        } else {
+            unreachable;
         }
     }
 };
